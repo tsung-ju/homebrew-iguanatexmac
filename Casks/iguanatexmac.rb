301e7732278e45f118bff5a2b29f360c3bd82b33
Tsung-Ju Chiang <71379180+tsung-ju@users.noreply.github.com>
2022-08-26 13:50:39 -0400

    Update iguanatexmac from 1.60 to 1.60.2

--- Casks/iguanatexmac.rb
+++ Casks/iguanatexmac.rb
@@ -1,13 +1,16 @@
 cask 'iguanatexmac' do
-  version '1.60'
-  sha256 '9ad8d8d3015085cb56ed289f07e5a8e7338ad19255addaa9271bbe4f7a8b2b2f'
+  version '1.60.2'
+  sha256 '6c59b3322e77073de547aabd63525152f11952d9a16efa95488cc476cdcec110'
 
   url "https://github.com/Jonathan-LeRoux/IguanaTex/releases/download/v#{version}/IguanaTex_v#{version.gsub('.', '_')}.zip"
   name 'IguanaTexMac'
   homepage 'https://github.com/Jonathan-LeRoux/IguanaTex'
 
-  artifact "IguanaTex_v#{version.gsub('.', '_')}.ppam",
-    target: "#{ENV['HOME']}/Library/Group Containers/UBF8T346G9.Office/User Content.localized/Add-Ins.localized/IguanaTex_v#{version.gsub('.', '_')}.ppam"
+  ppam_name = "IguanaTex_v#{version.gsub('.', '_')}.ppam"
+  ppam_dir = "#{ENV['HOME']}/Library/Group Containers/UBF8T346G9.Office/User Content.localized/Add-Ins.localized"
+
+  artifact ppam_name,
+    target: "#{ppam_dir}/#{ppam_name}"
   artifact "IguanaTex.scpt",
     target: "#{ENV['HOME']}/Library/Application Scripts/com.microsoft.Powerpoint/IguanaTex.scpt"
   artifact "libIguanaTexHelper.dylib",
@@ -17,7 +20,7 @@ cask 'iguanatexmac' do
     # Taken from http://youpresent.co.uk/developing-installers-for-office-mac-2016-application-add-ins/
     out, err, status = Open3.capture3('osascript', :stdin_data=>%Q{
       tell application "Microsoft PowerPoint"
-        set addIn to register add in "#{ENV['HOME']}/Library/Group Containers/UBF8T346G9.Office/User Content.localized/Add-Ins.localized/IguanaTex_v#{version.gsub('.', '_')}.ppam"
+        set addIn to register add in "#{ppam_dir}/#{ppam_name}"
         set the auto load of addIn to true
         set the loaded of addIn to true
       end tell
@@ -30,7 +33,7 @@ cask 'iguanatexmac' do
       tell application "Microsoft PowerPoint"
         if add ins is not missing value then
           repeat with addIn in (add ins as list)
-            if name of addIn = "IguanaTex_v#{version.gsub('.', '_')}" then
+            if name of addIn = "#{ppam_name}" then
               set loaded of addIn to false
               set auto load of addIn to false
               set registered of addIn to false
