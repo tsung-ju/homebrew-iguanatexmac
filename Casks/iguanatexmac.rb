b32eb44511e0cd28a405309be1337ae042a5d39e
Tsung-Ju Chiang <71379180+tsung-ju@users.noreply.github.com>
2026-09-13 19:05:35 +0800

    Use sh scripts for install and uninstall

--- Casks/iguanatexmac.rb
+++ Casks/iguanatexmac.rb
@@ -10,35 +10,23 @@ cask "iguanatexmac" do
   ppam_name = "#{addin_name}.ppam"
   ppam_dir = "#{Dir.home}/Library/Group Containers/UBF8T346G9.Office/User Content.localized/Add-Ins.localized"
 
-  # Taken from http://youpresent.co.uk/developing-installers-for-office-mac-2016-application-add-ins/
-  installer script: {
-    executable: "osascript",
-    input:      <<~SCRIPT,
+  generated_script "installer.sh", content: <<~SH
+    #!/bin/sh
+    mkdir -p "#{ppam_dir}"
+    cp "#{staged_path}/#{ppam_name}" "#{ppam_dir}"
+
+    # Taken from http://youpresent.co.uk/developing-installers-for-office-mac-2016-application-add-ins/
+    osascript << EOF
       tell application "Microsoft PowerPoint"
         set addIn to register add in "#{ppam_dir}/#{ppam_name}"
         set the auto load of addIn to true
         set the loaded of addIn to true
       end tell
-    SCRIPT
-  }
-  artifact "IguanaTex.scpt",
-           target: "#{Dir.home}/Library/Application Scripts/com.microsoft.Powerpoint/IguanaTex.scpt"
-  artifact "libIguanaTexHelper.dylib",
-           target: "/Library/Application Support/Microsoft/Office365/User Content.localized/Add-Ins.localized/libIguanaTexHelper.dylib"
-
-  preflight_steps do
-    run "xattr", args: ["-d", "com.apple.quarantine", "{{staged_path}}/libIguanaTexHelper.dylib"], must_succeed: false
-    copy ppam_name.to_s, "#{ppam_dir}/#{ppam_name}"
-  end
-
-  uninstall_postflight_steps do
-    remove "#{ppam_dir}/#{ppam_name}"
-    run "echo", args: ["Restart PowerPoint for the changes to take effect"]
-  end
-
-  uninstall script: {
-    executable:   "osascript",
-    input:        <<~SCRIPT,
+    EOF
+  SH
+  generated_script "uninstall.sh", content: <<~SH
+    #!/bin/sh
+    osascript << EOF
       tell application "Microsoft PowerPoint"
         if add ins is not missing value then
           repeat with addIn in (add ins as list)
@@ -50,7 +38,22 @@ cask "iguanatexmac" do
           end repeat
         end if
       end tell
-    SCRIPT
+    EOF
+
+    echo "Restart PowerPoint for the changes to take effect"
+  SH
+  installer script: "installer.sh"
+  artifact "IguanaTex.scpt",
+           target: "#{Dir.home}/Library/Application Scripts/com.microsoft.Powerpoint/IguanaTex.scpt"
+  artifact "libIguanaTexHelper.dylib",
+           target: "/Library/Application Support/Microsoft/Office365/User Content.localized/Add-Ins.localized/libIguanaTexHelper.dylib"
+
+  preflight_steps do
+    run "xattr", args: ["-d", "com.apple.quarantine", "{{staged_path}}/libIguanaTexHelper.dylib"], must_succeed: false
+  end
+
+  uninstall script: {
+    executable:   "uninstall.sh",
     must_succeed: false,
   }
 end
